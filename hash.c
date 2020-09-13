d04155ec607ea7f170cc63e750e39bf92038fef3
artem <artem@MacBook-Air-artem.local>
2020-09-14 01:33:44 +0300

    hash.c norminette

--- hash.c
+++ hash.c
@@ -6,84 +6,81 @@
 /*   By: nmustach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 14:44:25 by nmustach          #+#    #+#             */
-/*   Updated: 2020/07/23 23:56:55 by nmustach         ###   ########.fr       */
+/*   Updated: 2020/09/14 01:33:25 by nmustach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-t_hash	**hash_table_init()
+t_hash	**hash_table_init(void)
 {
-	size_t i;
-	t_hash **hash_table;
-	
+	size_t	i;
+	t_hash	**hash_table;
+
 	i = 0;
 	MFAIL((hash_table = malloc(sizeof(t_hash*) * TABLE_SIZE)));
-	while(i < TABLE_SIZE) // Заполнить массив NULL указателями
+	while (i < TABLE_SIZE)
 	{
 		hash_table[i] = NULL;
 		i++;
 	}
-	return(hash_table);
+	return (hash_table);
 }
 
 t_hash	*assign_to_table(t_hash **table, char *node_name)
 {
-	t_hash *new;
-	unsigned int hash_val; 
-	
-	hash_val = calc_hash(node_name);
-	
-		MFAIL((new = malloc(sizeof(t_hash))));
-		new->next = NULL;
-		new->child = NULL;
-		new->x = 0;
-		new->y = 0;
-		new->visit = 0;
-		new->bfs_level = INT_MAX;
-		new->node_name = node_name;
-		new->mark = INT_MAX;
-		new->prev = NULL;
+	t_hash			*new;
+	unsigned int	hash_val;
+	t_hash			*node;
 
-	if (table[hash_val] == NULL )
+	hash_val = calc_hash(node_name);
+	MFAIL((new = malloc(sizeof(t_hash))));
+	new->next = NULL;
+	new->child = NULL;
+	new->x = 0;
+	new->y = 0;
+	new->visit = 0;
+	new->bfs_level = INT_MAX;
+	new->node_name = node_name;
+	new->mark = INT_MAX;
+	new->prev = NULL;
+	if (table[hash_val] == NULL)
 		table[hash_val] = new;
 	else
 	{
-		t_hash *node;		
-		node = table[hash_val]; 
+		node = table[hash_val];
 		while (node->next)
 			node = node->next;
-		node->next = new;	
+		node->next = new;
 	}
 	return (new);
 }
 
 unsigned int	calc_hash(char *node_name)
 {
-	unsigned long int value;
-    unsigned int i;
+	unsigned long int	value;
+	unsigned int		i;
 
 	value = 0;
-		i = 0;
-	
+	i = 0;
 	while (node_name[i])
-			value = value * HASH_MULT + node_name[i++];
+		value = value * HASH_MULT + node_name[i++];
 	return (value % TABLE_SIZE);
 }
 
 void	free_hash_table(t_hash **h_table)
 {
-	size_t i;
-	t_hash *node;
-	t_hash *next;
-	t_child *child_list;
-	t_child *child_next;
-	
+	size_t	i;
+	t_hash	*node;
+	t_hash	*next;
+	t_child	*child_list;
+	t_child	*child_next;
+
 	i = 0;
-	while(i < TABLE_SIZE)
+	while (i < TABLE_SIZE)
 	{
 		if (h_table[i])
-		{		
+		{
 			node = h_table[i];
 			while (node)
 			{
@@ -108,18 +105,15 @@ void	free_hash_table(t_hash **h_table)
 	FCNT(free(h_table));
 }
 
-
 t_hash	*hash_query(t_hash **h_table, char *node_name)
 {
-	t_hash *node;
-	unsigned int hash_val;
+	t_hash			*node;
+	unsigned int	hash_val;
 
 	hash_val = calc_hash(node_name);
-	
-	if(h_table[hash_val] == NULL)
+	if (h_table[hash_val] == NULL)
 		return (NULL);
-	
-	node = h_table[hash_val];	
+	node = h_table[hash_val];
 	while (node)
 	{
 		if (ft_strequ(node_name, node->node_name))
--- parse_functions.c
+++ parse_functions.c
@@ -6,94 +6,95 @@
 /*   By: nmustach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 17:08:45 by nmustach          #+#    #+#             */
-/*   Updated: 2020/07/23 19:09:26 by nmustach         ###   ########.fr       */
+/*   Updated: 2020/09/14 01:22:17 by nmustach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-t_graph	*graph_init()
+t_graph	*graph_init(void)
 {
-	t_graph *graph;
-	
+	t_graph	*graph;
+
 	MFAIL((graph = malloc(sizeof(t_graph))));
 	graph->start = NULL;
 	graph->end = NULL;
 	graph->h_table = hash_table_init();
-	return(graph);
+	return (graph);
 }
 
 int	parse_start_node(char *line, t_graph *graph)
 {
-	t_hash *node;
-	char *str_ret;
-	
+	t_hash	*node;
+	char	*str_ret;
+
 	if (ft_strequ("##start", line))
-			{
-				if (graph->start)
-					err_exit();
-				if(((str_ret = gnl(graph->map_buf)) != NULL) && (node = parse_node_name(str_ret, graph->h_table)))
-				{
-						graph->start = node;
-						return (1);
-				}
-				else
-					err_exit();
-			}
+	{
+		if (graph->start)
+			err_exit();
+		if (((str_ret = gnl(graph->map_buf)) != NULL)
+		&& (node = parse_node_name(str_ret, graph->h_table)))
+		{
+			graph->start = node;
+			return (1);
+		}
+		else
+			err_exit();
+	}
 	return (0);
 }
 
 int	parse_end_node(char *line, t_graph *graph)
 {
-	t_hash *node;
+	t_hash	*node;
 	char	*str_ret;
-	
+
 	if (ft_strequ("##end", line))
+	{
+		if (graph->end)
+			err_exit();
+		if (((str_ret = gnl(graph->map_buf)) != NULL)
+		&& (node = parse_node_name(str_ret, graph->h_table)))
 		{
-			if (graph->end)
-					err_exit();
-				if(((str_ret = gnl(graph->map_buf)) != NULL) && (node = parse_node_name(str_ret, graph->h_table)))
-				{
-						graph->end = node;
-						return (1);
-				}
-				else
-					err_exit();
-			}
-			return (0);
+			graph->end = node;
+			return (1);
+		}
+		else
+			err_exit();
+	}
+	return (0);
 }
 
 void	check_if_already_linked(t_hash *haystack, t_hash *needle)
-{		
-	t_child *c_node_hay;
-	
+{
+	t_child	*c_node_hay;
+
 	c_node_hay = haystack->child;
-	
 	while (c_node_hay)
 	{
 		if (c_node_hay->c_node == needle)
-				err_exit();			
+			err_exit();
 		c_node_hay = c_node_hay->next;
 	}
 }
 
 void	add_link(t_hash *parent, t_hash *child)
-{			
-		t_child *new;
-		t_child *c_list;
+{
+	t_child *new;
+	t_child *c_list;
 
-		MFAIL((new = malloc(sizeof(t_child)))); 
-		new->next = NULL;
-		new->c_node = child;
-		new->flow = 1;
-		new->weight = 1;
-		if (parent->child == NULL)
-			parent->child = new;	
-		else
-		{
-			c_list = parent->child;
-		while(c_list->next)
+	MFAIL((new = malloc(sizeof(t_child))));
+	new->next = NULL;
+	new->c_node = child;
+	new->flow = 1;
+	new->weight = 1;
+	if (parent->child == NULL)
+		parent->child = new;
+	else
+	{
+		c_list = parent->child;
+		while (c_list->next)
 			c_list = c_list->next;
 		c_list->next = new;
-		}
+	}
 }
